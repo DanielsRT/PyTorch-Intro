19b6aa318b49a57051cf8c449b70ab27999d0d60
Robert Daniels <robdaniels2001@gmail.com>
2024-12-18 20:55:32 -0500

    Show tensor attributes

--- tensors.py
+++ tensors.py
@@ -11,14 +11,12 @@ x_data = torch.tensor(data)
 np_array = np.array(data)
 x_np = torch.from_numpy(np_array)
 
-
 #Tensors created from another tensor retains the properties unless overwritten
 x_ones = torch.ones_like(x_data) # retains the properties of x_data
 print(f"Ones Tensor: \n {x_ones} \n")
 
 x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
 print(f"Random Tensor: \n {x_rand} \n")
-
 
 # 'shape' is a tupe of tensor dimensions. It can be used as an argument to set the dimensions of an output tensor
 shape = (2,3,)
@@ -29,3 +27,13 @@ zeros_tensor = torch.zeros(shape)
 print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
+
+
+## Tensor Attributes
+
+#Tensor attributes describe their shape, datatype, and the device on which they are stored
+tensor = torch.rand(3,4)
+
+print(f"\nShape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
