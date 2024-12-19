b5dfe23b17c12d92cdca6786e7f88f583a74eedc
Robert Daniels <robdaniels2001@gmail.com>
2024-12-18 19:45:18 -0500

    Create tensor from data

--- tensors.py
+++ tensors.py
@@ -1,2 +1,10 @@
+import torch
+import numpy as np
+
+#Initializing tensors
+
+#Tensors can be created directly from data. Data type is inferred
+data = [[1, 2],[3, 4]]
+x_data = torch.tensor(data)
 
 
