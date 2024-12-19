90ec538a570c3e3b751c91a0d027dbb6318c52fa
Robert Daniels <robdaniels2001@gmail.com>
2024-12-18 20:04:33 -0500

    Create tensor with constant dimensions

--- tensors.py
+++ tensors.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-#Initializing tensors
+## Initializing Tensors
 
 #Tensors can be created directly from data. Data type is inferred
 data = [[1, 2],[3, 4]]
@@ -11,9 +11,21 @@ x_data = torch.tensor(data)
 np_array = np.array(data)
 x_np = torch.from_numpy(np_array)
 
+
 #Tensors created from another tensor retains the properties unless overwritten
 x_ones = torch.ones_like(x_data) # retains the properties of x_data
 print(f"Ones Tensor: \n {x_ones} \n")
 
 x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
 print(f"Random Tensor: \n {x_rand} \n")
+
+
+# 'shape' is a tupe of tensor dimensions. It can be used as an argument to set the dimensions of an output tensor
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
