a095641c5cabb886e2183f266dd37d90208b33ca
Robert Daniels <robdaniels2001@gmail.com>
2024-12-18 21:22:33 -0500

    Tensor matrix multiplication

--- tensors.py
+++ tensors.py
@@ -53,10 +53,25 @@ if torch.cuda.is_available():
 tensor = torch.ones(4,4)
 print(f"\nFirst row: {tensor[0]}")
 print(f"First column: {tensor[:, 0]}")
-print(f"Last column: {tensor[..., -1]}")
+print(f"Last column: {tensor[..., -1]}\n")
 tensor[:,1] = 0
 print(tensor)
 
 #Use torch.cat to concatenate a sequence of tensors along a given dimension.
 t1 = torch.cat([tensor, tensor, tensor], dim=1)
 print(t1)
+
+#Compute the matrix multiplication between two tensors. y1, y2, y3 will have the same value
+#'tensor.T' returns the transpose of a tensor
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+
+y3 = torch.rand_like(y1)
+torch.matmul(tensor, tensor.T, out = y3)
+
+#Compute the element-wise product. z1, z2, z3 will have the same value
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
+
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
