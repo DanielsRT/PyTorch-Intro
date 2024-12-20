a4690dea159abef237ac0ad00a8c9573ddd102d0
Robert Daniels <robdaniels2001@gmail.com>
2024-12-19 20:44:44 -0500

    Visualize samples of the training data

--- datasets.py
+++ datasets.py
@@ -1,14 +1,14 @@
-#Load the 'FashionMNIST' dataset 
-# root is the path where the train/test data is stored, 
-# train specifies training or test dataset, 
-# download=True downloads data from the internet if it isn't available at root, 
-# transform and target_transform specify the feature and label transformations
-
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
+
+#Load the 'FashionMNIST' dataset 
+# root is the path where the train/test data is stored, 
+# train specifies training or test dataset, 
+# download=True downloads data from the internet if it isn't available at root, 
+# transform and target_transform specify the feature and label transformations
 
 training_data = datasets.FashionMNIST(
     root="data",
@@ -23,3 +23,31 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
+
+##Iterating and Visualizing the Dataset
+
+#You can index Datasets manualy like a list: 'training_data[index]'. We can use 'matplotlib' 
+# to visualize some samples in our training data.
+
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
