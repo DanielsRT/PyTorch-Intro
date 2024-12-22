2b4bc51e3b9a05d5c602ca572cb0b1fff554b390
Robert Daniels <robdaniels2001@gmail.com>
2024-12-21 20:29:30 -0500

    Get device for training

--- model.py
+++ model.py
@@ -4,3 +4,14 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
+##Get Device for Training
+
+#We want to train our model on a hardware accelerator like the GPU or MPS. 
+# Check if available, otherwise use the CPU.
+
+device = (
+    "cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
