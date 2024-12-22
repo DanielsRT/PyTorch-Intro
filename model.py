cce62f6f369a625ff327ac26f217ff714dc5692a
Robert Daniels <robdaniels2001@gmail.com>
2024-12-21 20:57:06 -0500

    Run model with input

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ device = (
     else "mps" if torch.backends.mps.is_available()
     else "cpu"
 )
-print(f"Using {device} device")
+print(f"Using {device} device\n")
 
 ##Define the Class
 
@@ -42,3 +42,17 @@ class NeuralNetwork(nn.Module):
 #Create an instance of NeuralNetwork, move it to the device, and print its structure.
 model  = NeuralNetwork().to(device)
 print(model)
+
+#To use the model, pass it the input data. This executes the model's forward, along 
+# with some background operations. Do not call model.forward(directly).
+
+#Calling the model on the input returns a 2-d tensor with dim=0 corresponding to each 
+# output of 10 raw predicted values for each class, and dim=1 corresponding to the 
+# individual values of each output. We get the prediction probabilities by passing it 
+# through an instance of the nn.Softmax module.
+
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
