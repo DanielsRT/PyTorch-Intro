2d7ca96ea8a6e78bc34e78c38747557fe5fb6ce7
Robert Daniels <robdaniels2001@gmail.com>
2024-12-22 23:30:09 -0500

    Compute jacobian products

--- autograd.py
+++ autograd.py
@@ -65,3 +65,23 @@ print(z.requires_grad)
 z = torch.matmul(x, w)+b
 z_det = z.detach()
 print(z_det.requires_grad)
+
+#In many cases, we have a scalar loss function, and we need to compute the gradient with 
+# respect to some parameters. However, there are cases when the output function is an 
+# arbitrary tensor. In this case, PyTorch allows you to compute so-called Jacobian product, 
+# and not the actual gradient.
+
+#Instead of computing the Jacobian matrix itself, PyTorch allows you to compute Jacobian 
+# Product for a given input vector v = (v1 ... vm). This is achieved by calling backward 
+# with as v an argument. The size of v should be the same as the size of the original tensor, 
+# with respect to which we want to compute the product:
+
+inp = torch.eye(4, 5, requires_grad=True)
+out = (inp+1).pow(2).t()
+out.backward(torch.ones_like(out), retain_graph=True)
+print(f"First call\n{inp.grad}")
+out.backward(torch.ones_like(out), retain_graph=True)
+print(f"\nSecond call\n{inp.grad}")
+inp.grad.zero_()
+out.backward(torch.ones_like(out), retain_graph=True)
+print(f"\nCall after zeroing gradients\n{inp.grad}")
